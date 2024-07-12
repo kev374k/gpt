0d57dad0bacacd9a5f2d06bccd5cf82a91a79c70
kev374k <kev374k@gmail.com>
2024-07-12 10:56:08 -0700

    adding positional encoding functionality

--- gpt_final_version.py
+++ gpt_final_version.py
@@ -14,6 +14,7 @@ N_EMBD = 384
 N_HEAD = 6
 N_LAYER = 6
 DROPOUT = 0.1
+MAX_LENGTH = 5000
 
 # ----------------------
 
@@ -66,15 +67,15 @@ class GenerateText:
         """
         return "".join([self.idx_to_char[i] for i in x])
 
-    def get_batch(self, split):
+    def get_batch(self, split, block_size = BLOCK_SIZE, batch_size = BATCH_SIZE, device = DEVICE):
         """
         Used to get a batch of data for batch gradient descent
         """
         batch_data = self.train_data if split == "train" else self.val_data
-        ix = torch.randint(len(batch_data) - BLOCK_SIZE, (BATCH_SIZE,))
-        xb = torch.stack([batch_data[i : i + BLOCK_SIZE] for i in ix])
-        yb = torch.stack([batch_data[i + 1 : i + BLOCK_SIZE + 1] for i in ix])
-        xb, yb = xb.to(DEVICE), yb.to(DEVICE)
+        ix = torch.randint(len(batch_data) - block_size, (batch_size,))
+        xb = torch.stack([batch_data[i : i + block_size] for i in ix])
+        yb = torch.stack([batch_data[i + 1 : i + block_size + 1] for i in ix])
+        xb, yb = xb.to(device), yb.to(device)
         return xb, yb
 
     @torch.no_grad()
@@ -98,30 +99,99 @@ class Head(nn.Module):
     """
     A single head of self-attention
     """
-    def __init__(self, head_size: int, block_size: int = BLOCK_SIZE, dropout: float = DROPOUT):
+    def __init__(self, head_size: int, n_embd: int = N_EMBD, block_size: int = BLOCK_SIZE,\
+                  dropout: float = DROPOUT):
         """
         Initialization of the Head Class
         """
         super().__init__()
-        self.key = nn.Linear(N_EMBD, head_size, bias = False)
-        self.query = nn.Linear(N_EMBD, head_size, bias = False)
-        self.value = nn.Linear(N_EMBD, head_size, bias = False)
+        self.key = nn.Linear(n_embd, head_size, bias = False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        B, T, C = x.shape
+        """
+        Forward pass for the Head Module
+        """
+        t, c = x.shape[1:]
         q, k, v = self.query(x), self.key(x), self.value(x)
 
         # compute attention scores
-        w = q @ k.transpose(-2, 1) * C ** -0.5
-        wei = w.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        w = q @ k.transpose(-2, 1) * c ** -0.5
+        wei = w.masked_fill(self.tril[:t, :t] == 0, float('-inf'))
         wei = F.softmax(wei, dim = -1)
 
         # perform weighted aggregiation using our values
         output = wei @ v
         return output
-    
-class Multi
 
+class MultiHeadedAttention(nn.Module):
+    """
+    Multiple Heads of Self-Attention in Parallel
+    """
+    def __init__(self, num_heads, head_size, n_embd = N_EMBD, dropout = DROPOUT):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        Forward pass for the MultiHeadedAttention class
+        """
+        out = torch.cat([h(x) for h in self.heads])
+        out = self.proj(out)
+        return out
+
+class FeedForwardNetwork(nn.Module):
+    """
+    Feed Forward Network with ReLU activation in between
+    """
+    def __init__(self, n_embd = N_EMBD):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd * 4),
+            nn.ReLU(),
+            nn.Linear(n_embd * 4, n_embd),
+        )
+
+    def forward(self, x):
+        """
+        Forward pass for the feed forward network
+        """
+        return self.net(x)
+
+class Block(nn.Module):
+    """
+    Block that encapsulates all the previous neural network layers into one
+    """
+    def __init__(self, n_embd = N_EMBD, n_head = N_HEAD):
+        super().__init__()
+        head_size = n_embd // n_head
+
+        # self-attention layer
+        self.sa = MultiHeadedAttention(n_embd, head_size)
+
+        # feed forward network initalization
+        self.ffwd = FeedForwardNetwork(n_embd)
+
+        # initialization of the two layer norms used in the structure
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        """
+        Forward pass for the Block Class
+        """
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+class PositionalEncoder(nn.Module):
+    """
+    Adds positional encoding functionality for the language model
+    """
+    def __init__(self, d_model, dropout = DROPOUT, max_len = MAX_LENGTH):
